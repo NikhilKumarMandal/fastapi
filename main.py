69f0ea9868a30991ec65cadaf20ed6ce9c198275
Nikhil <kumarnikhil48578@gmail.com>
2025-07-05 11:41:35 +0530

    feat(view): add fetch all patients data

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 from fastapi import FastAPI  
+import json
 
 app = FastAPI()
 
-
+def load_data():
+    with open("patients.json","r") as f:
+        data = json.load(f)
+    return data
 
 @app.get("/")
 def hello():
-    return {'message': 'Hello world'}
+    return {'message': 'Patient Management System Api'}
 
 
 @app.get("/about")
 def about():
-    return {"message": "Hello my name is nikhil"}
+    return {"message": "A fully functional API to manage your patient records"}
+
+@app.get("/view")
+def view():
+    data = load_data()
+
+    return data
